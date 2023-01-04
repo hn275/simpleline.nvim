bfb59e5d853236b69b115b88bd6f25929db546c9
Hal <haln_01@proton.me>
2023-01-04 13:28:30 -0800

    fix: regex matched for git

--- lua/simpleline/modules/git.lua
+++ lua/simpleline/modules/git.lua
@@ -1,10 +1,15 @@
-return function()
+local get_git_branch = function()
 	local branch = vim.api.nvim_exec("echo FugitiveStatusline()", true)
-	local branch_name = branch:gmatch("%((%a+)%)")()
+	local branch_name = branch:gmatch("%((.+)%)")()
 
 	if branch_name ~= nil then
 		branch_name = "  " .. branch_name
+		return branch_name
 	end
 
-	return branch_name or ""
+	return ""
 end
+
+print(get_git_branch())
+
+return get_git_branch
